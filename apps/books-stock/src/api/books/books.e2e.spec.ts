d7590c7543026dcf1fc38411763a3419c5b506b8
toli444 <toli44777@gmail.com>
2023-11-05 13:10:03 +0100

    tests: add first test for books route

--- apps/books-stock/src/api/books/books.e2e.spec.ts
+++ apps/books-stock/src/api/books/books.e2e.spec.ts
@@ -1,5 +1,6 @@
 import request from "supertest";
 import { app, prisma } from "../../server";
+import { clearDB } from "../../utils/test.util";
 
 const ROUTE = "/books";
 
@@ -16,14 +17,49 @@ describe("Books", () => {
         }
       }
     });
+
+    await prisma.book.create({
+      data: {
+        name: "Nineteen Eighty-Four",
+        author: {
+          create: {
+            firstName: "George",
+            lastName: "Orwell"
+          }
+        }
+      }
+    });
   });
 
   afterAll(async () => {
+    await clearDB();
     await prisma.$disconnect();
   });
 
   test("/GET books", async () => {
     const res = await request(app).get(ROUTE);
     expect(res.statusCode).toBe(200);
+    expect(res.body).toEqual([
+      {
+        author: {
+          firstName: "Paulo",
+          id: 1,
+          lastName: "Coelho"
+        },
+        authorId: 1,
+        id: 1,
+        name: "The Alchemist"
+      },
+      {
+        author: {
+          firstName: "George",
+          id: 2,
+          lastName: "Orwell"
+        },
+        authorId: 2,
+        id: 2,
+        name: "Nineteen Eighty-Four"
+      }
+    ]);
   });
 });
---
+++ apps/books-stock/src/utils/test.util.ts
@@ -0,0 +1,6 @@
+import { exec } from "child_process";
+import * as util from "util";
+
+const execPromisify = util.promisify(exec);
+
+export const clearDB = async () => execPromisify("npm run test:db:reset");
